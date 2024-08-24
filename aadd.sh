7a1486f257c74455d6a28e720f8012e16ab98f05
Chintu <neelamdheerajkumar1122@gmail.com>
2024-08-24 11:44:09 +0530

    hi from add

---
+++ aadd.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+
+# Ask user for first number
+echo "Enter first number: 20"
+
+# Ask user for second number
+echo "Enter second number: 10"
+
+# Perform addition
+sum=$((20 + 10))
+
+# Display result
+echo "Sum: $sum"
